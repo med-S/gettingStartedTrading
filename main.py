1d2de5ad4a2b4aaef86b2274cf8d85d2a5918661
Mohamed SALHAOUI <50855585+med-S@users.noreply.github.com>
2022-01-11 18:34:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import logging
+import os
+from flask import Flask
 
-print("test")
-logging.basicConfig(filename="logFile",
-                            filemode='a',
-                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                            datefmt='%H:%M:%S',
-                            level=logging.DEBUG)
+app = Flask(__name__) # create Flask server
+# get instance of logger and set log severity as defined by the cli parameter
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
 
-logging.info("Running Urban Planning")
+@app.route('/')
+def hello():
+    app.logger.debug('This is a DEBUG log record.')
+    app.logger.info('This is an INFO log record.')
+    app.logger.warning('This is a WARNING log record.')
+    app.logger.error('This is an ERROR log record.')
+    app.logger.critical('This is a CRITICAL log record.')
+    return 'Hello world!'
