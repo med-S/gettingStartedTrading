c50e8190727bc530654f71b68d8bdf8bbc7a4d34
Mohamed SALHAOUI <50855585+med-S@users.noreply.github.com>
2022-01-11 18:46:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ gunicorn_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers = gunicorn_logger.handlers
 app.logger.setLevel(gunicorn_logger.level)
 
-@app.route('https://getstartedtrading.herokuapp.com/', methods=('GET'))
+@app.route('https://getstartedtrading.herokuapp.com/', methods=["POST"])
 def hello():
     app.logger.debug('This is a DEBUG log record.')
     app.logger.info('This is an INFO log record.')
     app.logger.warning('This is a WARNING log record.')
     app.logger.error('This is an ERROR log record.')
     app.logger.critical('This is a CRITICAL log record.')
+    print("TEST")
     return 'Hello world!'
